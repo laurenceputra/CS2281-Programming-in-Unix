ff36c3586766d6141431100884158befdf4b96fd
laurenceputra <laurenceputra@gmail.com>
2013-02-28 00:07:37 +0800

    memory optimal one done

---
+++ Lab3/U096833E-lab3-part1-v1.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define BUFFER_SIZE 32
+
+int main(){
+	char buffer[BUFFER_SIZE];
+	size_t curBufferSize;
+	size_t lineSize = 0; //null string
+	char *line = malloc(sizeof(char) * 1);
+	int palindrome, loopLimit, counter;
+
+	while(fgets(buffer, BUFFER_SIZE, stdin)){
+		curBufferSize = strlen(buffer);
+		lineSize += curBufferSize;
+		line = realloc(line, lineSize);
+		if(line == NULL){
+			printf("Realloc failed!\n");
+			free(line);
+			exit(-1);
+		}
+		strcat(line, buffer);
+		if((curBufferSize > 0 && buffer[curBufferSize - 1] == '\n') || feof(stdin)){
+			if(curBufferSize > 0 && buffer[curBufferSize - 1] == '\n'){
+				lineSize--;
+				line[lineSize] = '\0';
+			}
+			if (lineSize == 0){
+				continue;
+			}
+			else{
+				lineSize--;
+				loopLimit = lineSize / 2;
+				palindrome = 0;
+				for (counter = 0; counter < loopLimit; counter++){
+					if(line[counter] != line[lineSize - counter]){
+						palindrome = 1;
+						break;
+					}
+				}
+				if (palindrome == 0){
+					fputs(line, stdout);
+					fputs("\n", stdout);
+				}
+			}
+			free(line);
+			lineSize = 0;
+			line = malloc(sizeof(char) * 1);
+		}
+	}
+}
