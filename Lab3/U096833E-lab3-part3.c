17564bb9fdda5f83e87316d490d00218ec7d99e7
laurenceputra <laurenceputra@gmail.com>
2013-03-06 13:21:58 +0800

    completed

--- Lab3/U096833E-lab3-part3.c
+++ Lab3/U096833E-lab3-part3.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+typedef struct record {
+    int account;
+    char name[30];
+    int date;
+    int month;
+    int year;
+    int padding;
+    char op[5];
+    long long transaction;
+    long long amount_left;
+} record;
 
 int main(int argc, char *argv[]){
 	if(argc != 2){
@@ -8,10 +20,27 @@ int main(int argc, char *argv[]){
 	FILE *binFile = fopen(argv[1], "rb");
 	char buffer[64];
 	unsigned char temp[64];
-	int i = 0, tmp;
-	do{
-		tmp = fgetc(binFile);
-		printf("%c", tmp);
-	}while(tmp != EOF);
+	char tmp;
+	int i = 0, data[64];
+	record *cur = malloc(sizeof(record));
+	while(!feof(binFile)){
+		fread(&cur->account, 1, 4, binFile);
+		if(feof(binFile)){
+			break;
+		}
+		fread(cur->name, 1, 30, binFile);
+		fread(&cur->date, 1, 1, binFile);
+		fread(&cur->month, 1, 1, binFile);
+		fread(&cur->year, 1, 2, binFile);
+		fread(&cur->padding, 1, 2, binFile);
+		fread(cur->op, 1, 4, binFile);
+		cur->op[4] = '\0';
+		fread(&cur->padding, 1, 4, binFile);
+		fread(&cur->transaction, 1, 8, binFile);
+		fread(&cur->amount_left, 1, 8, binFile);
+		printf("%d %s\n", cur->account, cur->name);
+		printf("%d %d %d\n", cur->date, cur->month, cur->year);
+		printf("%s %ld %ld\n DONE!\n", cur->op, cur->transaction, cur->amount_left);
+	}
 	return 0;
 }
