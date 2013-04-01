e6b312508f926ce99b544b9f0ed2498ca09ae28c
laurenceputra <laurenceputra@gmail.com>
2013-04-01 23:22:12 +0800

    updated q2-bonus

--- Lab6/U096833E-lab6-part2.c
+++ Lab6/U096833E-lab6-part2.c
@@ -61,7 +61,7 @@ int main(){
 	while((file = readdir(proc)) != NULL){
 		//process only actual files
 		if(file->d_name[0] != '.'){
-			filename = strcat(strdup("/proc/"), strcat(strdup(file->d_name), "/psinfo"));
+			sprinf(filename, "/proc/%s/psinfo", file->d_name);
 			procFile = fopen(filename, "r");
 			//process only files you can open
 			if(procFile != NULL){
